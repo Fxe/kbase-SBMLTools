7615807147717c6f091013b908ea1adabad7efe6
fliu <fliu@deb.uminho.pt>
2017-04-10 12:03:49 -0500

    try save mock model 2 (added a bunch of empty fields)

--- lib/src/sbmltools/SbmlTools.java
+++ lib/src/sbmltools/SbmlTools.java
@@ -73,6 +73,15 @@ public class SbmlTools {
     this.workspace = workspace;
   }
   
+  public static ModelCompound mockCompound(String ref, String cmp) {
+    return new ModelCompound().withId(ref)
+        .withCompoundRef(ref)
+        .withModelcompartmentRef(cmp)
+        .withFormula("R")
+        .withCharge(1.0)
+        .withName(ref + "@" + cmp);
+  }
+  
   public static Object mockModel() {
     String cmp = "default";
     FBAModel model = new FBAModel();
@@ -83,7 +92,14 @@ public class SbmlTools {
     modelreactions.add(new ModelReaction()
         .withId("rxn00148")
         .withName("ATP:pyruvate O2-phosphotransferase")
+        .withDirection("=")
+        .withProtons(1.0)
+        .withReactionRef("rxn00148")
+        .withModelReactionProteins(new ArrayList<ModelReactionProtein> ())
+        .withProbability(1.0)
+        .withModelcompartmentRef(cmp)
         .withModelReactionReagents(Arrays.asList(
+            
             new ModelReactionReagent().withCoefficient(-1d).withModelcompoundRef("cpd00002"),
             new ModelReactionReagent().withCoefficient(-1d).withModelcompoundRef("cpd00020"),
             new ModelReactionReagent().withCoefficient(1d).withModelcompoundRef("cpd00008"),
@@ -91,21 +107,33 @@ public class SbmlTools {
             new ModelReactionReagent().withCoefficient(1d).withModelcompoundRef("cpd00067"))));
     
     List<ModelCompound> modelcompounds = new ArrayList<> ();
-    modelcompounds.add(new ModelCompound().withId("cpd00002").withModelcompartmentRef(cmp));
-    modelcompounds.add(new ModelCompound().withId("cpd00008").withModelcompartmentRef(cmp));
-    modelcompounds.add(new ModelCompound().withId("cpd00020").withModelcompartmentRef(cmp));
-    modelcompounds.add(new ModelCompound().withId("cpd00061").withModelcompartmentRef(cmp));
-    modelcompounds.add(new ModelCompound().withId("cpd00067").withModelcompartmentRef(cmp));
+    modelcompounds.add(mockCompound("cpd00002", cmp));
+    modelcompounds.add(mockCompound("cpd00008", cmp));
+    modelcompounds.add(mockCompound("cpd00020", cmp));
+    modelcompounds.add(mockCompound("cpd00061", cmp));
+    modelcompounds.add(mockCompound("cpd00067", cmp));
     
     List<ModelCompartment> modelcompartments = new ArrayList<> ();
-    modelcompartments.add(new ModelCompartment().withId(cmp).withLabel("default"));
+    modelcompartments.add(
+        new ModelCompartment().withId(cmp)
+        .withLabel("default")
+        .withPH(7.3)
+        .withPotential(1.0)
+        .withCompartmentIndex(1L)
+        .withCompartmentRef(cmp));
     
     
     model.setModelreactions(modelreactions);
     model.setModelcompounds(modelcompounds);
     model.setModelcompartments(modelcompartments);
     model.setGenomeRef("4345/13/39");
-    
+    model.setSource("");
+    model.setSourceId("");
+    model.setType("");
+    model.setTemplateRef("");
+    model.setGapfillings(new ArrayList<ModelGapfill> ());
+    model.setGapgens(new ArrayList<ModelGapgen> ());
+    model.setBiomasses(new ArrayList<Biomass> ());
     return model;
   }
   
