41c55f3b4606af915907def619df7c74d1e65736
fliu <fliu@deb.uminho.pt>
2017-08-24 16:12:26 -0500

    ident

--- lib/src/sbmltools/SBMLToolsServer.java
+++ lib/src/sbmltools/SBMLToolsServer.java
@@ -58,20 +58,20 @@ import pt.uminho.sysbio.biosynthframework.kbase.KBaseSbmlImporter.ImportModelRes
  * </pre>
  */
 public class SBMLToolsServer extends JsonServerServlet {
-    private static final long serialVersionUID = 1L;
-    private static final String version = "0.0.1";
-    private static final String gitUrl = "https://fxe@github.com/Fxe/kbase-SBMLTools.git";
-    private static final String gitCommitHash = "69f06c693ba2e96dc0d65b6fdeaf37d566af2900";
+  private static final long serialVersionUID = 1L;
+  private static final String version = "0.0.1";
+  private static final String gitUrl = "https://fxe@github.com/Fxe/kbase-SBMLTools.git";
+  private static final String gitCommitHash = "69f06c693ba2e96dc0d65b6fdeaf37d566af2900";
 
-    //BEGIN_CLASS_HEADER
+  //BEGIN_CLASS_HEADER
   private static final Logger logger = LoggerFactory.getLogger(SBMLToolsServer.class);
   private final URL callbackURL;
   private final Path scratch;
-    //END_CLASS_HEADER
+  //END_CLASS_HEADER
 
-    public SBMLToolsServer() throws Exception {
-        super("SBMLTools");
-        //BEGIN_CONSTRUCTOR
+  public SBMLToolsServer() throws Exception {
+    super("SBMLTools");
+    //BEGIN_CONSTRUCTOR
     final String sdkURL = System.getenv("SDK_CALLBACK_URL");
     try {
       callbackURL = new URL(sdkURL);
@@ -85,24 +85,24 @@ public class SBMLToolsServer extends JsonServerServlet {
     } else {
       scratch = Paths.get(super.config.get("scratch"));
     }
-        //END_CONSTRUCTOR
-    }
+    //END_CONSTRUCTOR
+  }
 
-    /**
-     * <p>Original spec-file function name: sbml_importer</p>
-     * <pre>
-     * The actual function is declared using 'funcdef' to specify the name
-     * and input/return arguments to the function.  For all typical KBase
-     * Apps that run in the Narrative, your function should have the 
-     * 'authentication required' modifier.
-     * </pre>
-     * @param   params   instance of type {@link sbmltools.SbmlImporterParams SbmlImporterParams}
-     * @return   parameter "output" of type {@link sbmltools.SbmlImporterResults SbmlImporterResults}
-     */
-    @JsonServerMethod(rpc = "SBMLTools.sbml_importer", async=true)
-    public SbmlImporterResults sbmlImporter(SbmlImporterParams params, AuthToken authPart, RpcContext jsonRpcContext) throws Exception {
-        SbmlImporterResults returnVal = null;
-        //BEGIN sbml_importer
+  /**
+   * <p>Original spec-file function name: sbml_importer</p>
+   * <pre>
+   * The actual function is declared using 'funcdef' to specify the name
+   * and input/return arguments to the function.  For all typical KBase
+   * Apps that run in the Narrative, your function should have the 
+   * 'authentication required' modifier.
+   * </pre>
+   * @param   params   instance of type {@link sbmltools.SbmlImporterParams SbmlImporterParams}
+   * @return   parameter "output" of type {@link sbmltools.SbmlImporterResults SbmlImporterResults}
+   */
+  @JsonServerMethod(rpc = "SBMLTools.sbml_importer", async=true)
+  public SbmlImporterResults sbmlImporter(SbmlImporterParams params, AuthToken authPart, RpcContext jsonRpcContext) throws Exception {
+    SbmlImporterResults returnVal = null;
+    //BEGIN sbml_importer
     final String workspaceName = params.getWorkspaceName();
 
 
@@ -129,13 +129,13 @@ public class SBMLToolsServer extends JsonServerServlet {
 
     List<String> files = new ArrayList<> ();
     files.add("index.html");
-    files.add("css/bootstrap.min.css");
+//    files.add("css/bootstrap.min.css");
     files.add("js/jquery-2.2.2.min.js");
     files.add("js/underscore-min.js");
     files.add("js/plotly-1.28.3.min.js");
-    files.add("ids.json");
-    files.add("names.json");
-    files.add("refs.json");
+//    files.add("ids.json");
+//    files.add("names.json");
+//    files.add("refs.json");
     List<String> datas = new ArrayList<> ();
 
     for (String f : files) {
@@ -175,76 +175,76 @@ public class SBMLToolsServer extends JsonServerServlet {
       //name instead of ref!
       returnVal.withFbamodelId(result.modelName);
     }
-        //END sbml_importer
-        return returnVal;
-    }
+    //END sbml_importer
+    return returnVal;
+  }
 
-    /**
-     * <p>Original spec-file function name: integrate_model</p>
-     * <pre>
-     * </pre>
-     * @param   params   instance of type {@link sbmltools.IntegrateModelParams IntegrateModelParams}
-     * @return   parameter "output" of type {@link sbmltools.SbmlImporterResults SbmlImporterResults}
-     */
-    @JsonServerMethod(rpc = "SBMLTools.integrate_model", async=true)
-    public SbmlImporterResults integrateModel(IntegrateModelParams params, AuthToken authPart, RpcContext jsonRpcContext) throws Exception {
-        SbmlImporterResults returnVal = null;
-        //BEGIN integrate_model
-    
+  /**
+   * <p>Original spec-file function name: integrate_model</p>
+   * <pre>
+   * </pre>
+   * @param   params   instance of type {@link sbmltools.IntegrateModelParams IntegrateModelParams}
+   * @return   parameter "output" of type {@link sbmltools.SbmlImporterResults SbmlImporterResults}
+   */
+  @JsonServerMethod(rpc = "SBMLTools.integrate_model", async=true)
+  public SbmlImporterResults integrateModel(IntegrateModelParams params, AuthToken authPart, RpcContext jsonRpcContext) throws Exception {
+    SbmlImporterResults returnVal = null;
+    //BEGIN integrate_model
+
     System.out.println(config);
     System.out.println(callbackURL);
-    
+
     final String workspaceName = params.getWorkspaceName();
-    
+
     final DataFileUtilClient dfuClient = new DataFileUtilClient(callbackURL, authPart);
     final KBaseReportClient  kbrClient = new KBaseReportClient(callbackURL, authPart);
     final WorkspaceClient    wspClient = new WorkspaceClient(new URL(config.get("workspace-url")), authPart);
     final KBSolrUtilClient  solrClient = new KBSolrUtilClient(callbackURL, authPart);
-//    wspClient.setServiceVersion("beta");
+    //    wspClient.setServiceVersion("beta");
     dfuClient.setIsInsecureHttpConnectionAllowed(true);
     kbrClient.setIsInsecureHttpConnectionAllowed(true);
     wspClient.setIsInsecureHttpConnectionAllowed(true);
     solrClient.setServiceVersion("beta");
     solrClient.setIsInsecureHttpConnectionAllowed(true);
-    
+
 
     KBaseGeneIntegration geneIntegration = new KBaseGeneIntegration(wspClient, dfuClient, solrClient);
-    
-//    KBaseIOUtils.getFBAModel2(params.getModelName(), workspaceName, null, wspClient);
+
+    //    KBaseIOUtils.getFBAModel2(params.getModelName(), workspaceName, null, wspClient);
     returnVal = new KBaseModelIntegrationFacade(wspClient, 
-                                                dfuClient, 
-                                                kbrClient,
-                                                geneIntegration,
-                                                "/data/integration/export").kbaseIntegrate(params, workspaceName);
-    
-        //END integrate_model
-        return returnVal;
-    }
-    @JsonServerMethod(rpc = "SBMLTools.status")
-    public Map<String, Object> status() {
-        Map<String, Object> returnVal = null;
-        //BEGIN_STATUS
+        dfuClient, 
+        kbrClient,
+        geneIntegration,
+        "/data/integration/export").kbaseIntegrate(params, workspaceName);
+
+    //END integrate_model
+    return returnVal;
+  }
+  @JsonServerMethod(rpc = "SBMLTools.status")
+  public Map<String, Object> status() {
+    Map<String, Object> returnVal = null;
+    //BEGIN_STATUS
     returnVal = new LinkedHashMap<String, Object>();
     returnVal.put("state", "OK");
     returnVal.put("message", "");
     returnVal.put("version", version);
     returnVal.put("git_url", gitUrl);
     returnVal.put("git_commit_hash", gitCommitHash);
-        //END_STATUS
-        return returnVal;
-    }
+    //END_STATUS
+    return returnVal;
+  }
 
-    public static void main(String[] args) throws Exception {
-        if (args.length == 1) {
-            new SBMLToolsServer().startupServer(Integer.parseInt(args[0]));
-        } else if (args.length == 3) {
-            JsonServerSyslog.setStaticUseSyslog(false);
-            JsonServerSyslog.setStaticMlogFile(args[1] + ".log");
-            new SBMLToolsServer().processRpcCall(new File(args[0]), new File(args[1]), args[2]);
-        } else {
-            System.out.println("Usage: <program> <server_port>");
-            System.out.println("   or: <program> <context_json_file> <output_json_file> <token>");
-            return;
-        }
+  public static void main(String[] args) throws Exception {
+    if (args.length == 1) {
+      new SBMLToolsServer().startupServer(Integer.parseInt(args[0]));
+    } else if (args.length == 3) {
+      JsonServerSyslog.setStaticUseSyslog(false);
+      JsonServerSyslog.setStaticMlogFile(args[1] + ".log");
+      new SBMLToolsServer().processRpcCall(new File(args[0]), new File(args[1]), args[2]);
+    } else {
+      System.out.println("Usage: <program> <server_port>");
+      System.out.println("   or: <program> <context_json_file> <output_json_file> <token>");
+      return;
     }
+  }
 }
