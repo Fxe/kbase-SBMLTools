bdeb19e66dc9dbf9de3ac90e955b8ae9d526493c
fliu <fliu@deb.uminho.pt>
2017-09-06 00:21:31 -0500

    wrong import

--- lib/src/pt/uminho/sysbio/biosynthframework/kbase/AutoPropagateGenomeFacade.java
+++ lib/src/pt/uminho/sysbio/biosynthframework/kbase/AutoPropagateGenomeFacade.java
@@ -11,7 +11,6 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.biojava.nbio.core.sequence.DNASequence;
 import org.biojava.nbio.core.sequence.io.FastaReaderHelper;
-import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -90,7 +89,7 @@ public class AutoPropagateGenomeFacade {
       } else {
         logger.warn("unable to find feature");
       }
-    } catch (IOException | JSONException | JsonClientException e) {
+    } catch (IOException | JsonClientException e) {
       e.printStackTrace();
     }
   }
